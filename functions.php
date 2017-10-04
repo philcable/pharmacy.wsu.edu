3a19a9af412c6320c6d2a29d248475acbfea0eb5
Jeremy Felt <jeremy.felt@gmail.com>
2017-10-04 10:29:54 -0700

    Bump version 0.0.2

--- functions.php
+++ functions.php
@@ -1,6 +1,18 @@
 <?php
 
+add_filter( 'spine_child_theme_version', 'pharmacy_theme_version' );
 add_action( 'init', 'pharmacy_register_footer_menu' );
+
+/**
+ * Provides a theme version for use in cache busting.
+ *
+ * @since 0.0.2
+ *
+ * @return string
+ */
+function pharmacy_theme_version() {
+	return '0.0.2';
+}
 
 /**
  * Registers the menu locations for the site footer.
