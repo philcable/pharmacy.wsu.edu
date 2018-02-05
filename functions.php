6c40d0c1fcae2d906796d377a17baa0f2274f466
Phil Cable <pcable@wsu.edu>
2018-02-05 11:52:20 -0800

    Filter the rewrite slug used for people

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/includes/content-syndicate.php';
 
 add_filter( 'spine_child_theme_version', 'pharmacy_theme_version' );
 add_action( 'init', 'pharmacy_register_footer_menu' );
+add_filter( 'wsuwp_people_default_rewrite_slug', 'pharmacy_people_rewrite_arguments' );
 
 /**
  * Provides a theme version for use in cache busting.
@@ -23,4 +24,18 @@ function pharmacy_theme_version() {
  */
 function pharmacy_register_footer_menu() {
 	register_nav_menu( 'footer', 'Footer' );
+}
+
+/**
+ * Filter the rewrite arguments passed to register_post_type by the people directory.
+ *
+ * @param array|bool $rewrite False by default. Array if previously filtered.
+ *
+ * @return array
+ */
+function pharmacy_people_rewrite_arguments( $rewrite ) {
+	return array(
+		'slug' => 'directory',
+		'with_front' => false,
+	);
 }
